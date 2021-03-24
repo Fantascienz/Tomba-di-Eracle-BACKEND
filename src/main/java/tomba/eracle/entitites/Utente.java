c7c22997d2dd88a9c86f1b499e3414042a6f8227
Paolo Marchitto <paolo.marchitto88@gmail.com>
2021-03-24 17:35:36 +0100

    Implmentazione lista personaggi di un utente da risolvere problema CORS

--- src/main/java/tomba/eracle/entitites/Utente.java
+++ src/main/java/tomba/eracle/entitites/Utente.java
@@ -1,10 +1,13 @@
 package tomba.eracle.entitites;
 
+import java.util.List;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 import lombok.Data;
@@ -30,5 +33,8 @@ public class Utente {
 	
 	@Column(name = "tipo")
 	private String tipo;
+	
+	@OneToMany(mappedBy = "utente")
+	private List<Personaggio> personaggi;
 	
 }
