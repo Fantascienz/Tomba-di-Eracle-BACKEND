e6d23350726751774f17d0aaa8f2440528509357
Tiziano Massa <tiziano.max@gmail.com>
2021-03-25 21:46:47 +0100

    cambiato import di Transient

--- src/main/java/tomba/eracle/entitites/Utente.java
+++ src/main/java/tomba/eracle/entitites/Utente.java
@@ -9,8 +9,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
-
-import org.springframework.data.annotation.Transient;
+import javax.persistence.Transient;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
