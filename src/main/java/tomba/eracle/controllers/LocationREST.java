3aff49c6d2470b210b079d38b55e074dd4a44966
Tiziano Massa <tiziano.max@gmail.com>
2021-03-31 10:21:03 +0200

    aggiunti metodi per trovare locations con direzioni libere

--- src/main/java/tomba/eracle/controllers/LocationREST.java
+++ src/main/java/tomba/eracle/controllers/LocationREST.java
@@ -1,10 +1,12 @@
 package tomba.eracle.controllers;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,5 +24,26 @@ public class LocationREST {
 	@CrossOrigin
 	public List<Location> getAllMacroLocations () {
 		return (List<Location>) locationRepo.findByMappa("Macro");
+	}
+	
+	@GetMapping("/{direzioneLibera}")
+	@CrossOrigin
+	public List<Location> getLocationByDirezioneNull (@PathVariable("direzioneLibera") String direzioneLibera) {
+		List<Location> locations = new ArrayList<Location>();
+		switch(direzioneLibera) {
+		case "nord":
+			locations = locationRepo.findByNordNull();
+			break;
+		case "est":
+			locations = locationRepo.findByEstNull();
+			break;
+		case "sud":
+			locations = locationRepo.findBySudNull();
+			break;
+		case "ovest":
+			locations = locationRepo.findByOvestNull();
+			break;
+		}
+		return locations;
 	}
 }
